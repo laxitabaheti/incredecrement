8fad6363c739e53792946fe302e0b13a7cbecd6f
Laxita <86618594+laxitabaheti@users.noreply.github.com>
2021-10-15 00:46:14 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+function increment() {
+  document.getElementById("incdecre").innerHTML =
+    parseInt(document.getElementById("incdecre").innerHTML) + 1;
+}
+function decrement() {
+  document.getElementById("incdecre").innerHTML =
+    parseInt(document.getElementById("incdecre").innerHTML) - 1;
+}
